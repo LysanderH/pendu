f987a3a31018a10601b962a6e5dee6dd053acfae
Lysander <lysander.hans@hotmail.com>
2019-02-20 11:20:29 +0100

    initial

--- views/partials/form.php
+++ views/partials/form.php
@@ -1,12 +1,17 @@
-<form  action="index.php" method="get">
-<fieldset>
-    <legend>
-        Il te reste X essais pour sauver ta peau
-    </legend>
-    <label for="triedLetter">Choisis ta lettre</label>
-    <select name="" id="triedLetter">
-        <option value="x">X</option>
-    </select>
-    <input type="submit" value="Essayer la lettre">
-</fieldset>
+<form action="index.php" method="post">
+    <fieldset>
+        <legend>
+            Il te reste X essais pour sauver ta peau
+        </legend>
+        <label for="triedLetter">Choisis ta lettre</label>
+        <select name="triedLetter" id="triedLetter">
+            <?php foreach ($lettersArray as $letter => $status): ?>
+                <?php if ($status): ?>
+                    <option value="<?= $letter; ?>"><?= $letter; ?></option>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </select>
+        <input type="hidden" name="lettersArray" value="<?= $serializedLettersArray; ?>">
+        <input type="submit" value="Essayer la lettre">
+    </fieldset>
 </form>
