0534300d4f3f5d9c339c0cc62f379f3aa95ed1a6
Lysander <lysander.hans@hotmail.com>
2019-02-20 12:11:52 +0100

    Process words.txt file

--- configs/config.php
+++ configs/config.php
@@ -2,3 +2,8 @@
 
 define('MAX_TRIALS', 8);
 define('GAME_TITLE', 'Le pendu');
+define('FILE_DIRECTORY', 'data');
+define('FILE_NAME', 'words.txt');
+define('FILE_PATH_SEPARATOR', '/');
+define('FILE_PATH', FILE_DIRECTORY . FILE_PATH_SEPARATOR . FILE_NAME);
+
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+
 include 'configs/config.php';
+$wordsArray = file(FILE_PATH);
+var_dump($wordsArray);
+
+
 include 'controllers/controller.php';
 include 'views/layout.html';
